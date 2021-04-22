315811005893b53a22035e7bc85985941f97aae7
farzanreza <73737752+farzanreza@users.noreply.github.com>
2021-04-23 00:08:38 +0530

    Update and rename index.php to index.html

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    echo file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,
-+Mountain+View,+CA&key=AIzaSyDti8hQd5MRMq-5LEtLI_1eF-pOjsdTF6U");
-?>
-
-
-
